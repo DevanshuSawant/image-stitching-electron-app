af2f907964e9e516be02db035390b2efe3a48969
DevanshuSawant <76646274+DevanshuSawant@users.noreply.github.com>
2023-06-25 14:10:38 +0530

    edge case with tour button during processing fixed + prettier

--- src/autostitch.js
+++ src/autostitch.js
@@ -1,14 +1,13 @@
 // const fs = require('fs');
 
-
 let options_exe = {
-    mode : 'text',
-    pythonPath: 'C:\\Windows\\System32\\WindowsPowerShell\\v1.0\\powershell.exe',
+  mode: "text",
+  pythonPath: "C:\\Windows\\System32\\WindowsPowerShell\\v1.0\\powershell.exe",
 };
 
 let options_py = {
-    mode : 'text',
-    pythonOptions: ['-u'], // get print results in real-time
+  mode: "text",
+  pythonOptions: ["-u"], // get print results in real-time
 };
 
 // possible type of messages passed by python code to javascript
@@ -16,289 +15,309 @@ let options_py = {
 // cd = current number of images currently processed
 // er = error message to see if manual stitching is required
 // fd = file directory of the stitched image
-let {PythonShell} = require('python-shell')
-// let pyshell = new PythonShell("./src/engine/upload_multiple.py", options_py);  // for when py is converted to exe
-let pyshell = new PythonShell("./resources/app/src/upload_multiple.exe", options_exe);  // for when py is converted to exe
+let { PythonShell } = require("python-shell");
+let pyshell = new PythonShell("./src/engine/upload_multiple.py", options_py); // for when py is converted to exe
+// let pyshell = new PythonShell("./resources/app/src/upload_multiple.exe", options_exe);  // for when py is converted to exe
 
 fileNames = [];
 hasPythonCodeRun = false;
-var imageUpload = document.getElementById('image-upload');
-imageUpload.addEventListener('change', function(event) {
+hasPythonCodeStarted = false;
+var imageUpload = document.getElementById("image-upload");
+imageUpload.addEventListener("change", function (event) {
   // Reset state to before python code was run
-  const path = require('path');
+  const path = require("path");
   hasPythonCodeRun = false;
   var files = event.target.files;
   for (var i = 0; i < files.length; i++) {
-      fileNames.push(files[i].path);
+    fileNames.push(files[i].path);
   }
   console.log(event.target.files);
   pyshell.send(fileNames);
   console.log(fileNames);
-  
+
+  hasPythonCodeStarted = true;
   pythonRunner();
-  var imageUploadLabel = document.getElementById('uploadtour');
+  var imageUploadLabel = document.getElementById("uploadtour");
   imageUploadLabel.classList.add("d-none");
 
   // imageUpload.disabled = true;
 });
 
-
-  
 function pythonRunner() {
-    // console.log(pyPaths);
+  // console.log(pyPaths);
 
-    pyshell.on('message', function (message) {
-      var progressBarParent = document.getElementById("progress-bar-parent");
-      progressBarParent.classList.remove("d-none");
+  pyshell.on("message", function (message) {
+    var progressBarParent = document.getElementById("progress-bar-parent");
+    progressBarParent.classList.remove("d-none");
+    console.log(message);
+    const [typeofmessage, messagecode] = message.split(":");
+
+    if (typeofmessage == "fd") {
       console.log(message);
-      const [typeofmessage, messagecode] = message.split(":");
+      console.log("stitched image saved");
+      let strippedPath = message.replace(/^fd:/, "");
+      // let outputMessage = document.getElementById('file-directory');
+      // outputMessage.innerHTML = "Stitched image will be saved at: " + strippedPath;
+      // fd = path.join(strippedPath, 'image1.png');
+      localStorage.setItem("finalImageFolder", strippedPath);
+      historyFolder = strippedPath.replace(
+        "result-images",
+        "result-images-history"
+      );
+      localStorage.setItem("historyFolder", historyFolder);
+    }
 
+    if (typeofmessage == "tn") {
+      tn = messagecode;
+      outputMessage = document.getElementById("python-output");
+      outputMessage.classList.remove("d-none");
+      outputMessage.innerHTML =
+        '<i class="bi bi-info-circle-fill"></i> ' +
+        tn +
+        " : Images Selected for Stitching";
+    }
 
-      if (typeofmessage == 'fd') {
-          console.log(message);
-          console.log('stitched image saved');
-          let strippedPath = message.replace(/^fd:/, '');
-          // let outputMessage = document.getElementById('file-directory');
-          // outputMessage.innerHTML = "Stitched image will be saved at: " + strippedPath;
-          // fd = path.join(strippedPath, 'image1.png');
-          localStorage.setItem('finalImageFolder', strippedPath);
-          historyFolder = strippedPath.replace('result-images', 'result-images-history');
-          localStorage.setItem('historyFolder', historyFolder);
-          
-      }
-      
-      if (typeofmessage == 'tn') {
-          tn=messagecode;
-          outputMessage = document.getElementById('python-output');
-          outputMessage.classList.remove("d-none");
-          outputMessage.innerHTML = '<i class="bi bi-info-circle-fill"></i> ' + tn + ' : Images Selected for Stitching';
-          
-      }
+    if (typeofmessage == "cd") {
+      cd = messagecode;
+      let percentageDone = (cd / tn) * 100;
+      console.log(percentageDone);
+      var progressBar = document.getElementById("progress-bar");
+      progressBar.setAttribute("aria-valuenow", percentageDone);
+      progressBar.style.width = percentageDone + "%";
+      // progressBar.value = percentageDone;
+      outputMessage = document.getElementById("python-output");
+      // outputMessage.classList.replace("alert-info", "alert-success");
+      outputMessage.innerHTML =
+        '<i class="bi bi-info-circle-fill"></i> ' +
+        cd +
+        " of " +
+        tn +
+        " Images Processed";
+    }
 
-      if (typeofmessage == 'cd') {
-          cd=messagecode;
-          let percentageDone = cd/tn*100;
-          console.log(percentageDone);
-          var progressBar = document.getElementById("progress-bar");
-          progressBar.setAttribute("aria-valuenow", percentageDone);
-          progressBar.style.width = percentageDone + "%";
-          // progressBar.value = percentageDone;
-          outputMessage = document.getElementById('python-output');
-          // outputMessage.classList.replace("alert-info", "alert-success");
-          outputMessage.innerHTML = '<i class="bi bi-info-circle-fill"></i> ' + cd + ' of ' + tn + ' Images Processed';
+    if (typeofmessage == "er") {
+      hasPythonCodeRun = true;
+      if (messagecode == 0) {
+        console.log(messagecode);
+        console.log("no stitching needed");
+        // console.log(fd);
+        var fileOpenButton = document.getElementById("file-open-button");
+        var fileCopyButton = document.getElementById("copy-path-button");
+        outputMessage = document.getElementById("python-output");
+        outputMessage.classList.replace("alert-info", "alert-success");
+        outputMessage.innerHTML =
+          '<i class="bi bi-check-lg"></i> All Images Stitched Successfully!';
+        fileOpenButton.style.display = "block";
+        fileCopyButton.style.display = "block";
+        finalImagePath = localStorage.getItem("finalImagePath");
+        const { shell } = require("electron"); // deconstructing assignment
+        shell.openPath(finalImagePath);
       }
+      if (messagecode == 1) {
+        console.log(messagecode);
+        console.log("needs stitching");
+        window.location.href = "autostitch-manualstitcher.html"; // Redirect to autostitcher-maualstitcher HTML file
+      }
+    }
+    if (typeofmessage == "finished") {
+      let strippedPath = message.replace(/^finished:/, "");
+      // shell.openPath(strippedPath);
+      localStorage.setItem("finalImagePath", strippedPath);
+    }
+  });
 
-      if (typeofmessage == 'er') {
-        hasPythonCodeRun = true;
-        if (messagecode==0) {
-            console.log(messagecode)
-            console.log('no stitching needed')
-            // console.log(fd);
-            var fileOpenButton = document.getElementById('file-open-button');
-            var fileCopyButton = document.getElementById('copy-path-button');
-            outputMessage = document.getElementById('python-output');
-            outputMessage.classList.replace("alert-info", "alert-success");
-            outputMessage.innerHTML = '<i class="bi bi-check-lg"></i> All Images Stitched Successfully!';
-            fileOpenButton.style.display = 'block';
-            fileCopyButton.style.display = 'block';
-            finalImagePath = localStorage.getItem('finalImagePath');
-            const {shell} = require('electron') // deconstructing assignment
-            shell.openPath(finalImagePath);
-        }
-        if (messagecode==1) {
-            console.log(messagecode)
-            console.log('needs stitching')
-            window.location.href = 'autostitch-manualstitcher.html';                 // Redirect to autostitcher-maualstitcher HTML file
-        }
-      }
-      if (typeofmessage == 'finished') {
-          let strippedPath = message.replace(/^finished:/, '');
-          // shell.openPath(strippedPath);
-          localStorage.setItem('finalImagePath', strippedPath);
-      }
-    });
-    
-    // end the input stream and allow the process to exit
-    pyshell.end(function (err,code,signal) {
+  // end the input stream and allow the process to exit
+  pyshell.end(function (err, code, signal) {
     if (err) throw err;
-    console.log('The exit code was: ' + code);
-    console.log('The exit signal was: ' + signal);
-    console.log('finished');
-    });
+    console.log("The exit code was: " + code);
+    console.log("The exit signal was: " + signal);
+    console.log("finished");
+  });
 }
 
-const {shell} = require('electron') // deconstructing assignment
+const { shell } = require("electron"); // deconstructing assignment
 
 showInFolder = () => {
-    let finalImageFolder = localStorage.getItem('finalImageFolder');
-    shell.openExternal(`file://${finalImageFolder}`);
-    
-    var fileOpenButton = document.getElementById('file-open-button');
-    fileOpenButton.innerHTML = '<i class="bi bi-folder-check"></i> Show Image In File Explorer';
-    setTimeout(function(){
-      fileOpenButton.innerHTML = '<i class="bi bi-folder"></i> Show Image In File Explorer';
-    },1000);
-}
+  let finalImageFolder = localStorage.getItem("finalImageFolder");
+  shell.openExternal(`file://${finalImageFolder}`);
+
+  var fileOpenButton = document.getElementById("file-open-button");
+  fileOpenButton.innerHTML =
+    '<i class="bi bi-folder-check"></i> Show Image In File Explorer';
+  setTimeout(function () {
+    fileOpenButton.innerHTML =
+      '<i class="bi bi-folder"></i> Show Image In File Explorer';
+  }, 1000);
+};
 
 copyImageToClipBoard = () => {
-  var fileCopyButton = document.getElementById('copy-path-button');
-  fileCopyButton.innerHTML = '<i class="bi bi-clipboard-check"></i> Copy Image to Clipboard';
-  setTimeout(function(){
-      fileCopyButton.innerHTML = '<i class="bi bi-clipboard"></i> Copy Image to Clipboard';
-  },1000);
-  finalImagePath = localStorage.getItem('finalImagePath');
-  const { clipboard, nativeImage } = require('electron');
+  var fileCopyButton = document.getElementById("copy-path-button");
+  fileCopyButton.innerHTML =
+    '<i class="bi bi-clipboard-check"></i> Copy Image to Clipboard';
+  setTimeout(function () {
+    fileCopyButton.innerHTML =
+      '<i class="bi bi-clipboard"></i> Copy Image to Clipboard';
+  }, 1000);
+  finalImagePath = localStorage.getItem("finalImagePath");
+  const { clipboard, nativeImage } = require("electron");
   const image = nativeImage.createFromPath(finalImagePath);
   clipboard.writeImage(image);
-}
+};
 
 cancelProcess = () => {
-    pyshell.kill();
-    console.log('process cancelled');
-}
+  pyshell.kill();
+  console.log("process cancelled");
+};
 // Tour
 // Initialize the Shepherd.js Tour
-const Shepherd = require('shepherd.js')
+const Shepherd = require("shepherd.js");
 
 let tour = null;
 
-
 function startTour() {
   if (tour) {
-    var fileOpenButton = document.getElementById('file-open-button');
-    var fileCopyButton = document.getElementById('copy-path-button');
-    fileOpenButton.style.display = 'block';
-    fileCopyButton.style.display = 'block';
+    var fileOpenButton = document.getElementById("file-open-button");
+    var fileCopyButton = document.getElementById("copy-path-button");
+    var imageUploadLabel = document.getElementById("uploadtour");
+    imageUploadLabel.classList.remove("d-none");
+    fileOpenButton.style.display = "block";
+    fileCopyButton.style.display = "block";
     tour.start();
   }
 }
 
-  tour = new Shepherd.Tour({
-    useModalOverlay: true,
-  });
+tour = new Shepherd.Tour({
+  useModalOverlay: true,
+});
 
-  tourCancelAction = () => {
-    tour.cancel();
-    console.log('tour cancelled');
-    if (hasPythonCodeRun == false) {
-    var fileOpenButton = document.getElementById('file-open-button');
-    var fileCopyButton = document.getElementById('copy-path-button');
-    fileOpenButton.style.display = 'none';
-    fileCopyButton.style.display = 'none';
-    }
+tourCancelAction = () => {
+  tour.cancel();
+  console.log("tour cancelled");
+  if (hasPythonCodeRun == false) {
+    var fileOpenButton = document.getElementById("file-open-button");
+    var fileCopyButton = document.getElementById("copy-path-button");
+    fileOpenButton.style.display = "none";
+    fileCopyButton.style.display = "none";
   }
+  if (hasPythonCodeStarted == true) {
+    var imageUploadLabel = document.getElementById("uploadtour");
+    imageUploadLabel.classList.add("d-none");
+    // console.log("hi")
+  }
+};
 
-  steps=[{
-    id: 'step1',
-   title: 'Upload Images',
-    text: 'Click this button to upload your images to the automatic stitcher.',
-    attachTo: {
-      element: '#uploadtour',
-      on: 'bottom',
-    },
-    classes: 'step-class',
-    buttons: [
-      {
-        text: 'Next',
-        action: tour.next,
-      },
-      {
-        text: 'Exit Tour',
-        action: tourCancelAction
-      },
-    ],
-  },
+steps = [
   {
-    id: 'step2',
-    title: 'Copy Image',
-    text: 'After the image is stitched, this button will be shown,Click this to Copy the saved image to your clipboard.',
+    id: "step1",
+    title: "Upload Images",
+    text: "Click this button to upload your images to the automatic stitcher.",
     attachTo: {
-      element: '#copy-path-button',
-      on: 'right',
+      element: "#uploadtour",
+      on: "bottom",
     },
-    classes: 'step-class',
+    classes: "step-class",
     buttons: [
       {
-        text: 'Next',
+        text: "Next",
         action: tour.next,
       },
       {
-        text: 'Exit Tour',
-        action: tourCancelAction
-      },
-    ],
-  },
-  {
-    id: 'step3',
-    title: 'Open Folder',
-    text: 'After the image is stitched, this button will be shown,Click this to open the folder where the imaegs are saved.',
-    attachTo: {
-      element: '#file-open-button',
-      on: 'right',
-    },
-    classes: 'step-class',
-    buttons: [
-      {
-        text: 'Next',
-        action: tour.next,
-      },
-      {
-        text: 'Exit Tour',
+        text: "Exit Tour",
         action: tourCancelAction,
       },
     ],
   },
   {
-    id: 'step4',
-    title: 'Go Back',
-    text: 'Click this  button to Go back to Homepage.',
+    id: "step2",
+    title: "Copy Image",
+    text: "After the image is stitched, this button will be shown,Click this to Copy the saved image to your clipboard.",
     attachTo: {
-      element: '#backtour',
-      on: 'right',
+      element: "#copy-path-button",
+      on: "right",
     },
-    classes: 'step-class',
+    classes: "step-class",
     buttons: [
       {
-        text: 'Next',
+        text: "Next",
         action: tour.next,
       },
       {
-        text: 'Exit Tour',
-        action: tourCancelAction
+        text: "Exit Tour",
+        action: tourCancelAction,
       },
     ],
   },
   {
-    id: 'step5',
-    title: 'Tour Button',
-    text: 'In case you forget the button function, you can click this button again anytime and it will guide you through all the buttons.',
+    id: "step3",
+    title: "Open Folder",
+    text: "After the image is stitched, this button will be shown,Click this to open the folder where the imaegs are saved.",
     attachTo: {
-      element: '#tourtour',
-      on: 'top',
+      element: "#file-open-button",
+      on: "right",
     },
-    classes: 'step-class',
+    classes: "step-class",
     buttons: [
       {
-        text: 'Exit Tour',
-        action: tourCancelAction
-      },
-    ],
-  }
-];
-
-  tour.addSteps(steps);
-
-  tour.defaultStepOptions = {
-    classes: 'shepherd-theme-arrows',
-    scrollTo: true,
-    buttons: [
-      {
-        text: 'Next',
+        text: "Next",
         action: tour.next,
       },
       {
-        text: 'Exit Tour',
-        action: tourCancelAction
+        text: "Exit Tour",
+        action: tourCancelAction,
       },
     ],
-  };
+  },
+  {
+    id: "step4",
+    title: "Go Back",
+    text: "Click this  button to Go back to Homepage.",
+    attachTo: {
+      element: "#backtour",
+      on: "right",
+    },
+    classes: "step-class",
+    buttons: [
+      {
+        text: "Next",
+        action: tour.next,
+      },
+      {
+        text: "Exit Tour",
+        action: tourCancelAction,
+      },
+    ],
+  },
+  {
+    id: "step5",
+    title: "Tour Button",
+    text: "In case you forget the button function, you can click this button again anytime and it will guide you through all the buttons.",
+    attachTo: {
+      element: "#tourtour",
+      on: "top",
+    },
+    classes: "step-class",
+    buttons: [
+      {
+        text: "Exit Tour",
+        action: tourCancelAction,
+      },
+    ],
+  },
+];
+
+tour.addSteps(steps);
+
+tour.defaultStepOptions = {
+  classes: "shepherd-theme-arrows",
+  scrollTo: true,
+  buttons: [
+    {
+      text: "Next",
+      action: tour.next,
+    },
+    {
+      text: "Exit Tour",
+      action: tourCancelAction,
+    },
+  ],
+};
--- src/index.js
+++ src/index.js
@@ -1,10 +1,9 @@
-const { app, BrowserWindow, ipcMain } = require('electron');
-const path = require('path');
-const fs = require('fs');
-
+const { app, BrowserWindow, ipcMain } = require("electron");
+const path = require("path");
+const fs = require("fs");
 
 // Handle creating/removing shortcuts on Windows when installing/uninstalling.
-if (require('electron-squirrel-startup')) {
+if (require("electron-squirrel-startup")) {
   app.quit();
 }
 
@@ -13,26 +12,26 @@ const createWindow = () => {
   const mainWindow = new BrowserWindow({
     width: 800,
     height: 600,
-    show:false,
-    icon: './resources/app/src/assets/square-smt-logo.ico',
+    show: false,
+    icon: "./resources/app/src/assets/square-smt-logo.ico",
     webPreferences: {
-      preload: path.join(__dirname, 'preload.js'),
+      preload: path.join(__dirname, "preload.js"),
       nodeIntegration: true,
       contextIsolation: false,
       enableRemoteModule: true,
     },
   });
-  var splash = new BrowserWindow({ 
-    icon: './resources/app/src/assets/square-smt-logo.ico',
-    width: 640, 
-    height: 352, 
-    transparent: true, 
-    frame: false, 
-    alwaysOnTop: false, 
+  var splash = new BrowserWindow({
+    icon: "./resources/app/src/assets/square-smt-logo.ico",
+    width: 640,
+    height: 352,
+    transparent: true,
+    frame: false,
+    alwaysOnTop: false,
     resizable: false,
   });
-  
-  splash.loadFile('./src/splash.html');
+
+  splash.loadFile("./src/splash.html");
   splash.center();
   setTimeout(function () {
     splash.close();
@@ -42,43 +41,43 @@ const createWindow = () => {
   }, 9000);
 
   // and load the index.html of the app.
-  mainWindow.loadFile(path.join(__dirname, 'index.html'));
+  mainWindow.loadFile(path.join(__dirname, "index.html"));
   mainWindow.setMenuBarVisibility(false);
   // Open the DevTools.
   // mainWindow.webContents.openDevTools();
-
 };
 
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
-app.on('ready', createWindow);
+app.on("ready", createWindow);
 
 // Quit when all windows are closed, except on macOS. There, it's common
 // for applications and their menu bar to stay active until the user quits
 // explicitly with Cmd + Q.
-app.on('window-all-closed', () => {
-  if (process.platform !== 'darwin') {
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") {
     app.quit();
   }
 });
 
-ipcMain.on('getImages', (event) => {
-  const imagePaths = fs.readdirSync(path.join('result-images'))
-    .filter(file => file.endsWith('.jpg') || file.endsWith('.png'))
-    .map(file => path.join('result-images', file));
+ipcMain.on("getImages", (event) => {
+  const imagePaths = fs
+    .readdirSync(path.join("result-images"))
+    .filter((file) => file.endsWith(".jpg") || file.endsWith(".png"))
+    .map((file) => path.join("result-images", file));
 
-  event.reply('imagePaths', imagePaths);
+  event.reply("imagePaths", imagePaths);
 });
 
-ipcMain.on('get-path', (event) => {
-  const path = require('path');
-  const fs = require('fs');
-  const imagePaths = fs.readdirSync(path.join('result-images'))
-  ipcMain.send('get-path-reply', imagePaths);
+ipcMain.on("get-path", (event) => {
+  const path = require("path");
+  const fs = require("fs");
+  const imagePaths = fs.readdirSync(path.join("result-images"));
+  ipcMain.send("get-path-reply", imagePaths);
 });
 
-app.on('activate', () => {
+app.on("activate", () => {
   // On OS X it's common to re-create a window in the app when the
   // dock icon is clicked and there are no other windows open.
   if (BrowserWindow.getAllWindows().length === 0) {
@@ -88,4 +87,3 @@ app.on('activate', () => {
 
 // In this file you can include the rest of your app's specific main process
 // code. You can also put them in separate files and import them here.
-
